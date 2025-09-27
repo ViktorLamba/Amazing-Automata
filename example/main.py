1dd195710fb8504a54f42b9c439e64e01692787a
Dmitry <a.d1mon@yandex.ru>
2025-09-27 15:52:57 +0300

    dev: final commit

--- example/main.py
+++
@@ -1,8 +0,0 @@
-from app.hello import hello
-import time
-
-if __name__ == "__main__":
-    print("Starting app...")
-    while True:
-        hello()
-        time.sleep(5)  # выводить сообщение каждые 5 секунд
--- example/tests/test_main.py
+++
@@ -1,6 +0,0 @@
-from app.hello import hello
-
-def test_hello(capsys):
-    hello()
-    captured = capsys.readouterr()
-    assert "Hello, world!" in captured.out
